278b742f58c1ed18a64c57bd634e78756aacf10d
tehedor <sergiot1805@gmail.com>
2025-02-20 09:54:28 +0100

    create dkpg

--- app2/scripts/script1.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Este es el script 1 que se ejecutará al recibir una petición PUT válida.
-date >> /tmp/script1_ejecutado.log
-echo "Script 1 ejecutado" >> /tmp/script1_ejecutado.log
--- app2/scripts/script2.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Este es el script 2 que se ejecutará al recibir una petición PUT válida.
-uptime >> /tmp/script2_ejecutado.log
-echo "Script 2 ejecutado" >> /tmp/script2_ejecutado.log
