bd83f68d887d643bd0380146cd64640d0f899754
tehedor <sergiot1805@gmail.com>
2025-02-25 02:51:25 +0100

    new tmp

--- ossec_config/ossec_config.sh
+++ ossec_config/ossec_config.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Descargar ossec y instalar
 sudo apt -y install libz-dev libssl-dev libpcre2-dev build-essential libsystemd-dev
-curl -L -o /tmp/ossec.3.8.0.tar.gz https://github.com/ossec/ossec-hids/archive/refs/tags/3.8.0.tar.gz
-tar -xzvf /tmp/ossec.3.8.0.tar.gz
+curl -L -o ./ossec.3.8.0.tar.gz https://github.com/ossec/ossec-hids/archive/refs/tags/3.8.0.tar.gz
+tar -xzvf ./ossec.3.8.0.tar.gz
 ## Configuraciones para la instalacion
-cat <<EOF > /tmp/ossec-hids-3.8.0/install.cfg.sh
+cat <<EOF > ./ossec-hids-3.8.0/install.cfg.sh
 USER_LANGUAGE="en"
 USER_NO_STOP="y"
 USER_INSTALL_TYPE="local"
@@ -17,7 +17,7 @@ USER_ENABLE_FIREWALL_RESPONSE="y"
 USER_WHITE_LIST="127.0.0.1"
 EOF
 ## Instalacion
-sudo  /tmp/ossec-hids-3.8.0/install.sh
+sudo  ./ossec-hids-3.8.0/install.sh
 
 
 ## Configuracioens de ossec
